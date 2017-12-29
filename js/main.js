4a1d46b7462bdf70941e6f765c615cefebf6b2d1
Jerol Graves <jerolgraves@gmail.com>
2017-12-29 15:41:49 -0500

    cover photo change

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const myWork = [
     'desc': "This is from the codecademy course on building apps from scratch.",
     'image': {
       'desc': "",
-      'src': "../images/ravenous.png",
+      'src': "./images/ravenous.png",
       'comment': ""
     }
   },
@@ -19,17 +19,17 @@ const myWork = [
     'desc': "For this project, the inspiration came from a tutorial produced by Udemy which had the entire project rendered from the index.html page. I refactored it to use React and React-router.",
     'image': {
       'desc': "",
-      'src': "../images/weatherApp_recent.png",
+      'src': "./images/weatherApp_recent.png",
       'comment': ""
     }
   },
   {
-    'title': "Etsy Clone",
+    'title': "(G)Etsy",
     'href': " https://s3.amazonaws.com/etsypics/etsy1.png",
     'desc': "This project was a homework assignment that is intended to be a mock-up of the Etsy website.  It represents a learning point where I combined SCSS, HTML, React, Backbone and an API request to add content.",
     'image': {
       'desc': "",
-      'src': "../images/work-exam21.png",
+      'src': "./images/work-exam21.png",
       'comment': ""
     }
   },
@@ -40,7 +40,7 @@ const myWork = [
     'desc': "These are examples of files I created using the tutorial from HackerRank's 'Thirty Days of Code'.  My experience learning java taught me the beauty of object-oriented programming which has enhanced my understanding of javascript. The code for the java files can be found in my github repo.",
     'image': {
       'desc': "",
-      'src': "../images/java_files.png",
+      'src': "./images/java_files.png",
       'comment': ""
     }
   },
@@ -50,7 +50,7 @@ const myWork = [
     'desc': "These files represent a hodgepodge of projects that range from homework assignments to tutorials I took on-line.  The bulk of my Iron Yard training involved styling with HTML and CSS for the first four weeks.  The remainder of the time was spent on Javascript, jQuery, Backbone and React.  The code can be found in my github repos.",
     'image': {
       'desc': "",
-      'src': "../images/styling.png",
+      'src': "./images/styling.png",
       'comment': ""
     }
   }
